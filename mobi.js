2082858d8eba9db48b35adad3d942393d204de39
matthewkenworthy <64850988+matthewkenworthy@users.noreply.github.com>
2022-07-19 16:38:48 +0800

    Update mobi.js

--- mobi.js
+++ mobi.js
@@ -1,18 +1,18 @@
 let Analytics = Mobi2Go.Analytics;
 let Events = Mobi2Go.Analytics.Events;
 
+// When a user clicks the 'Next step' button
+Analytics.bind({
+    EVENTS.STARTED_CHECKOUT: function() {
+        window.alert(_formatProducts());
+    }
+})
+
 // Identify Customer in Klaviyo
 Analytics.bind({
     IDENTIFY_CUSTOMER: function(customer) {
        _learnq.push(['identify', {
           '$email': customer.email }]);
-    }
-})
-
-// When a user clicks the 'Next step' button
-Analytics.bind({
-    EVENTS.STARTED_CHECKOUT: function() {
-        window.alert(_formatProducts());
     }
 })
 
