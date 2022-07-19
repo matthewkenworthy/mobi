e11e1eaf06a8de989791b21ad613f80472b83c14
matthewkenworthy <64850988+matthewkenworthy@users.noreply.github.com>
2022-07-19 13:21:46 +0800

    Update mobi.js

--- mobi.js
+++ mobi.js
@@ -1,4 +1,20 @@
 let Analytics = Mobi2Go.Analytics;
 let Events = Mobi2Go.Analytics.Events;
 
-window.alert(Events);
+// When a product menu category is viewed
+Mobi2Go.Analytics.bind({
+    VIEWED_PRODUCT_CATEGORY: function(category) {
+        // Google tag manager
+        window.alert(category.name);
+        // Generic analytic service (such as Google Analytics)
+        //        acme_tracker.track('opened_category', {method: category[0].name});
+    }
+});
+
+// Helper methods
+function startSession() {
+    window.dataLayer.appName = Mobi2Go.app.getPlatform();
+    window.dataLayer.appId = Mobi2Go.headoffice.domain_name + '-' + Mobi2Go.app.getPlatform();
+    window.dataLayer.currencyCode = Mobi2Go.headoffice.currency_abrv;
+}
+
