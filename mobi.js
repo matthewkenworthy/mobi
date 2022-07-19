f26af9585c2adf7e224bec84a308af2239090138
matthewkenworthy <64850988+matthewkenworthy@users.noreply.github.com>
2022-07-19 16:48:01 +0800

    Update mobi.js

--- mobi.js
+++ mobi.js
@@ -7,27 +7,12 @@ Analytics.bind({
        _learnq.push(['identify', {
           '$email': customer.email }]);
     }
-})
+});
 
 // When a user clicks the 'Next step' button
-Mobi2Go.Analytics.bind({
-    EVENTS.STARTED_CHECKOUT: function() {
-        // Google tag manager
-        var ecommerce = {
-            checkout: {
-                actionField: {
-                    step: 1
-                },
-                products: [
-                    _formatProducts()
-                ]
-            }
-        };
-
-        _event('checkoutPaymentStep1', null, null, null, ecommerce);
-
-        // Generic analytic service (such as Google Analytics)
-        acme_tracker.track('start_checkout');
+Analytics.bind({
+    Events.STARTED_CHECKOUT: function() {
+        window.alert(_formatProducts());
     }
 });
 
